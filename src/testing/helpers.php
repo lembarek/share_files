ee5c5b50023ca18ecd72da5c8ea3b9436294ff83
lembarek <lembarek2011@gmail.com>
2017-03-02 17:00:11 +0100

    use ufactory in place of factory

--- src/testing/helpers.php
+++ src/testing/helpers.php
@@ -4,6 +4,6 @@ use Lembarek\ShareFiles\Models\File;
 
 function createFile($overs = [], $limit=1)
 {
-    return factory(File::class, $limit)->create($overs);
+    return ufactory(File::class, $limit)->create($overs);
 }
 
